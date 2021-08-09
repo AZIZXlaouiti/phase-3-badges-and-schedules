3566095451cd8195cac87c04b0a4ae4511a59af8
AZIZXlaouiti <azizrocke@gmail.com>
2021-08-09 17:25:24 -0400

    completeed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map! do |e|
+        e = "Hello, my name is #{e}."
+    end
+    arr
+ end
+
+def assign_rooms(arr)
+    arr.map!.with_index { |e,index| e = "Hello, #{e}! You'll be assigned to room #{index+1}!"}
+end    
+
+def printer(arr)
+    message = []
+        arr.map do  |e|
+          e = "Hello, my name is #{e}." 
+          message << e
+        end
+        arr.map.with_index do |e,index|
+          e = "Hello, #{e}! You'll be assigned to room #{index+1}!"
+          message << e
+         end
+        message
+   end  
+     
